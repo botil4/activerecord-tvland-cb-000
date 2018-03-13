aefc28f988681c6cebbd4a28a5dbd52b69a8d277
Travis Clarkson <tk1667@gmail.com>
2018-03-13 21:44:52 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
@@ -7,7 +8,8 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    characters.each do |char|
+    binding.pry
+    characters.all.each do |char|
       "#{char.name} - #{char.show.name}"
     end
   end
