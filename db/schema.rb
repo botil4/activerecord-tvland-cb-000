893db4c1ff343993fd1ff42252c7bdb2bfee59a8
Travis Clarkson <tk1667@gmail.com>
2018-03-13 21:58:24 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 6) do
+ActiveRecord::Schema.define(version: 5) do
 
   create_table "actors", force: :cascade do |t|
     t.text "first_name"
     t.text "last_name"
-    t.text "catchphrase"
   end
 
   create_table "characters", force: :cascade do |t|
